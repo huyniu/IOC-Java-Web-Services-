606b0fe1e406a6f1253fdf52be78684237b9ebdb
huyniu <dhuy271105@gmail.com>
2026-08-09 14:57:08 +0700

    first commit

---
+++ src/main/java/com/example/intershipms/IocJavaWebServicesApplication.java
@@ -0,0 +1,13 @@
+package com.example.intershipms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IocJavaWebServicesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IocJavaWebServicesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/intershipms/IocJavaWebServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.intershipms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IocJavaWebServicesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
